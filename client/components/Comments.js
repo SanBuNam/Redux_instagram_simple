0d0371ca7685a9a8744089f6c1b9ccf11ef771cc
David Lee <DavidLee8two7@gmail.com>
2019-04-16 07:12:20 -0700

    combined reducer

---
+++ client/components/Comments.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Comments = () => {
+  return <div>I'm the comments1!</div>;
+};
+
+export default Comments;
--- client/components/Single.js
+++ client/components/Single.js
@@ -1,8 +1,19 @@
 import React from "react";
+import Photo from "./Photo";
+// import comments
 
 const Single = React.createClass({
   render() {
-    return <div className="single-photo">i am single</div>;
+    const i = this.props.posts.findIndex(
+      (post) => post.code === this.props.params.postId
+    );
+    const post = this.props.posts[i];
+
+    return (
+      <div className="single-photo">
+        <Photo i={i} post={post} {...this.props} />
+      </div>
+    );
   }
 });
 
