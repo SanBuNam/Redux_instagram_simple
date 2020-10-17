c0a18aa38c7691b7b8598cf16a4aad69592fc3cb
David Lee <softdavidlee@gmail.com>
2020-10-16 20:51:07 -0700

    Update store.js

--- client/store.js
+++ client/store.js
@@ -2,15 +2,14 @@ import { createStore, compose } from "redux";
 import { syncHistoryWithStore } from "react-router-redux";
 import { browserHistory } from "react-router";
 
-// import the root reducer
 import rootReducer from "./reducers/index";
 
 import comments from "./data/comments";
 import posts from "./data/posts";
 
-// create an object for the default data
+
 const defaultState = {
-  posts, // es6 posts : posts,
+  posts,
   comments
 };
 
